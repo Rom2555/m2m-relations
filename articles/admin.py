3f53144c5ac18496dd9c4dd9d5af33f6f87ac970
Roman E <rk2555@yandex.ru>
2026-02-11 11:14:46 +0300

    Зарегестрирован Tag в admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Article)
