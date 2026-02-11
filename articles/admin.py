f90438788f5ef33bd43ec80a65bc2b92899ba999
Roman E <rk2555@yandex.ru>
2026-02-11 11:44:21 +0300

    Реализована проверка на наличие одного и только одного основного раздела

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Scope, Tag
 
@@ -7,11 +9,36 @@ from .models import Article, Scope, Tag
 class TagAdmin(admin.ModelAdmin):
     pass
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        
+        # Подсчитываем количество основных разделов, игнорируя удалённые
+        main_count = sum(
+            form.cleaned_data.get('is_main', False)
+            for form in self.forms
+            if not form.cleaned_data.get('DELETE', False)
+        )
+        
+        # Проверяем, что основной раздел ровно один
+        if self.forms and main_count != 1:
+            raise ValidationError('У статьи должен быть ровно один основной раздел')
+        
+        return super().clean()  # вызываем базовый код переопределяемого метода
 
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ScopeInline]
+
+
