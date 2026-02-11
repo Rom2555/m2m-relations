d0f9f6c4231675a075df17a90ef96ff93cbda59e
Roman E <rk2555@yandex.ru>
2026-02-11 11:09:02 +0300

    Обновлен views.py.Применена сортировка по дате публикации

--- articles/views.py
+++ articles/views.py
@@ -5,10 +5,11 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {'object_list': Article.objects.all()}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+
     ordering = '-published_at'
+    context = {'object_list': Article.objects.all(), ordering: ordering}
 
     return render(request, template, context)
